97716d85ede3ab12d901f7e3d654c0312d1532f1
dmytronauman <dmytro.nauman@eligamo.com>
2022-08-25 17:20:40 +0200

    add: first  calculator widget component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
 import "./App.css";
+import Calculator from "./widgets/components/Calculator";
 
 function App() {
   return (
     <div className="App">
+      <Calculator></Calculator>
+
       <div className="container glass"></div>
       <div className="container glass"></div>
       <div className="container glass"></div>
