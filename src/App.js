dc9fe92ac03f36abe2c3333162f29d523928acdc
dmytronauman <dmytro.nauman@eligamo.com>
2022-08-25 11:58:22 +0200

    first setup with basic app and a first div container for the widgets (glass look)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container glass"></div>
+      <div className="container glass"></div>
+      <div className="container glass"></div>
+    </div>
+  );
+}
+
+export default App;
