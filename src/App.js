1b44e535af3ed574789143c6baa59e7e304f39bf
dmytronauman <dmytro.nauman@eligamo.com>
2022-08-27 13:42:20 +0200

    add: beginning of weather component. api data called in and displayed from json.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Calculator from "./widgets/components/Calculator";
+import Weather from "./widgets/components/Weather";
 
 function App() {
   return (
     <div className="App">
       <Calculator></Calculator>
 
-      <div className="container glass"></div>
+      <div className="container glass">
+        <Weather />
+      </div>
       <div className="container glass"></div>
       <div className="container glass"></div>
     </div>
---
+++ src/widgets/components/Weather.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "../style/Weather.css";
+import sunny from "../../assets/sunny.jpg";
+import cloudy from "../../assets/cloudy.jpg";
+
+const Weather = () => {
+  const apiKey = "03a54ffa1ab7576cedf7e4a660a559fe";
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key == "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+        });
+    }
+  };
+
+  const loadIcon = (data) => {
+    if (data < 50) {
+      return sunny;
+    } else if (data > 50) {
+      return cloudy;
+    }
+  };
+
+  return (
+    <div>
+      <input
+        className="input"
+        placeholder="Enter City..."
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      />
+
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>Welcome! Please enter the city to see the weather of.</p>
+        </div>
+      ) : (
+        <div>
+          <p>{weatherData.name}</p>
+          <p>{Math.round(((weatherData.main.temp - 32) * 5) / 9)}°C</p>
+          <img src={loadIcon(weatherData.clouds.all)} />
+        </div>
+      )}
+    </div>
+  );
+};
+export default Weather;
