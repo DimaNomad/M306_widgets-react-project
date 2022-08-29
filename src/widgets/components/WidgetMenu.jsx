a7fac3b5b974d22e0d08e55b48f7becd2f517680
dmytronauman <dmytro.nauman@eligamo.com>
2022-08-29 16:55:22 +0200

    fix: ignore default eventHandling by pressing "Enter" in widgetMenu (doesnt close widget anymore). added stocks icon to widgetMenu.

--- src/widgets/components/WidgetMenu.jsx
+++ src/widgets/components/WidgetMenu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BsCalculatorFill } from "react-icons/bs";
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import { SiBitcoinsv } from "react-icons/si";
+import { AiOutlineStock } from "react-icons/ai";
 import { useState } from "react";
 import Calculator from "./Calculator";
 import Weather from "./Weather";
@@ -17,8 +18,12 @@ const WidgetMenu = () => {
     <div className="App">
       <div className="widgetMenu">
         <button
+          id="myInputID"
           className="widgetButton"
           onClick={() => setHiddenCalculator(!hiddenCalculator)}
+          onKeyDown={(e) => {
+            e.key === "Enter" && e.preventDefault();
+          }}
         >
           <BsCalculatorFill size={25} />
         </button>
@@ -33,6 +38,12 @@ const WidgetMenu = () => {
           onClick={() => setHiddenCrypto(!hiddenCrypto)}
         >
           <SiBitcoinsv size={25} />
+        </button>
+        <button
+          className="widgetButton"
+          onClick={() => setHiddenCrypto(!hiddenCrypto)}
+        >
+          <AiOutlineStock size={25} />
         </button>
       </div>
       {hiddenCalculator && <Calculator />}
